59bca10227ae50151e69c9d1559f8cb8383fb690
eb3831 <eb3831@bs.amalnet.k12.il>
2024-11-05 15:35:01 +0200

    design update + MainActivity code

--- app/src/main/java/com/example/listviewseries/Activity2.java
+++ app/src/main/java/com/example/listviewseries/Activity2.java
@@ -1,5 +1,6 @@
 package com.example.listviewseries;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -20,5 +21,8 @@ public class Activity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        Intent gi = getIntent();
+
     }
 }
--- app/src/main/java/com/example/listviewseries/MainActivity.java
+++ app/src/main/java/com/example/listviewseries/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.listviewseries;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText etA1;
+    EditText etQorD;
+    Switch sw;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -23,5 +31,32 @@ public class MainActivity extends AppCompatActivity
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+        etA1 = findViewById(R.id.etA1);
+        etQorD = findViewById(R.id.etQorD);
+        sw = findViewById(R.id.sw);
+    }
+
+    public void clickedSwitch(View view)
+    {
+        if (sw.isChecked())
+            etQorD.setHint("הכנס הפרש:");
+        else
+            etQorD.setHint("הכנס מכפיל:");
+    }
+
+
+    public void clickedNext(View view)
+    {
+        if (( etA1.getText().toString().isEmpty() )
+                ||( etQorD.getText().toString().isEmpty()))
+            Toast.makeText(this, "illegal action! please insert numbers.", Toast.LENGTH_SHORT).show();
+        else
+        {
+            Intent intent = new Intent(this, Activity2.class);
+            intent.putExtra("etQorD", String.valueOf(etQorD));
+            intent.putExtra("etA1", String.valueOf(etA1));
+            startActivity(intent);
+
+        }
     }
 }

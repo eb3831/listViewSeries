dfb6d4c3bea2623410e6c26d6f4cf4e2928cb06a
eb3831 <eb3831@bs.amalnet.k12.il>
2024-11-19 14:47:19 +0200

    finish

--- app/src/main/java/com/example/listviewseries/Activity2.java
+++ app/src/main/java/com/example/listviewseries/Activity2.java
@@ -6,6 +6,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,11 +14,11 @@ public class Activity2 extends AppCompatActivity implements AdapterView.OnItemCl
 {
     ListView lv;
     Intent gi;
-    double [] seriesArry;
+    double[] seriesArry;
     String[] disArray;
-    double a1, seriesSum, dORq;
+    double a1, seriesSum, difference;
     int seriesType;
-    double difference;
+    TextView tvDorQ, tvA1, tvDifference, tvSn, tvN;
     ArrayAdapter<String> adp;
 
     @Override
@@ -27,6 +28,11 @@ public class Activity2 extends AppCompatActivity implements AdapterView.OnItemCl
         setContentView(R.layout.activity_2);
 
         lv = findViewById(R.id.lv);
+        tvDorQ = findViewById(R.id.tvDorQ);
+        tvA1 = findViewById(R.id.tvA1);
+        tvDifference = findViewById(R.id.tvDifference);
+        tvSn = findViewById(R.id.tvSn);
+        tvN = findViewById(R.id.tvN);
 
         gi = getIntent();
         seriesType = getIntent().getIntExtra("seriesType", 0);
@@ -36,36 +42,99 @@ public class Activity2 extends AppCompatActivity implements AdapterView.OnItemCl
         seriesArry = new double[20];
         disArray = new String[20];
 
+
+        tvDifference.setText(String.valueOf(difference));
+        tvA1.setText(String.valueOf(a1));
+
+
         if (seriesType == 0)
         {
-
-             for ( int i = 1; i < 21; i++ )
-             {
-                 seriesArry[i-1] = a1 * Math.pow(difference, i - 1);
-                 disArray[i-1] = String.valueOf(seriesArry[i-1]);
-             }
-        }
-
-        else
-        {
-            for ( int i = 1; i < 21; i++ )
+            tvDorQ.setText("q:");
+            for (int i = 1; i < 21; i++)
             {
-                seriesArry[i-1] = a1 + ((i - 1) * difference);
-                disArray[i-1] = String.valueOf(seriesArry[i-1]);
+                seriesArry[i - 1] = a1 * Math.pow(difference, i - 1);
+                disArray[i - 1] =  differentView(seriesArry[i - 1]);
             }
         }
+        else
+        {
+            tvDorQ.setText("d:");
+            for (int i = 1; i < 21; i++)
+            {
+                seriesArry[i - 1] = a1 + ((i - 1) * difference);
+                disArray[i - 1] = differentView(seriesArry[i - 1]);
+            }
+        }
+
 
         lv.setOnItemClickListener(this);
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         adp = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, disArray);
         lv.setAdapter(adp);
+    }
 
+    public static String differentView(double term)
+    {
+        if (term % 1 == 0 &&
+                term < 10000 &&
+                term > -10000)
+            return String.valueOf((int) term);
+
+        if (term >= 10000 ||
+                term <= -10000)
+        {
+            int exponent = 0;
+            double coefficient = term;
+
+            while (Math.abs(coefficient) >= 10000)
+            {
+                coefficient /= 10;
+                exponent++;
+            }
+
+            return String.format("%d * 10^%d", (int) coefficient, exponent);
+        }
+
+        int exponent = 0;
+        double coefficient = term;
+
+        if ( Math.abs(term) >= 1 )
+        {
+            while ( Math.abs(coefficient) >= 10 )
+            {
+                coefficient /= 10;
+                exponent ++;
+            }
+        }
+        else
+        {
+            while (Math.abs(coefficient) < 1)
+            {
+                coefficient *= 10;
+                exponent--;
+            }
+        }
+
+        return String.format("%.3f * 10^%d", coefficient, exponent);
     }
 
     @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l)
+    public void onItemClick(AdapterView<?> adapterView, View view, int pos, long l)
     {
+        if ( seriesType == 0 )
+            seriesSum = (a1 * ( Math.pow( difference, pos + 1 ) - 1 )) / ( difference - 1 );
 
+        else
+            seriesSum = ( ( pos +1 ) * ( a1 + seriesArry[ pos + 1 ] )) / 2;
+
+        tvSn.setText( differentView( seriesSum ));
+        tvN.setText( String.valueOf(pos +1 ));
     }
+
+    public void clickedBack (View view)
+    {
+        finish();
+    }
+
 }
